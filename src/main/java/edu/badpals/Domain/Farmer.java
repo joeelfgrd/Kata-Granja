7ce830302cc7697376fc845b30e025790c4c7769
Joel <joelfigueiridomolares@gmail.com>
2024-06-10 10:13:27 +0200

    feat(Farmer.java): Crear clase farmer y mapear

---
+++ src/main/java/edu/badpals/Domain/Farmer.java
@@ -0,0 +1,27 @@
+package edu.badpals.Domain;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "t_farmer")
+
+public class Farmer {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "location")
+    private String location;
+
+    
+    
+}
